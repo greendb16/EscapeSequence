7c29c3d87a299ff727c673c110eab1c6b3fcaf70
greendb16 <greendb16@gmail.com>
2019-06-12 14:29:52 -0400

    final Square Commit

---
+++ src/EscapeSequence.java
@@ -0,0 +1,7 @@
+public class EscapeSequence {
+    public static void main(String[]args){
+        //Escape Sequences making a pretty square
+        System.out.println("********\n*      *\n*      *\n*      *\n*      *\n********")  ;
+        System.out.println("Look At My Beautiful Square");
+    }
+}
